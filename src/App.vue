16e56f21b0dc5e85c30d2605013634e7e17e6ae9
AhogeK <ahogek@gmail.com>
2026-04-04 03:45:12 +0800

    refactor(app): remove hardcoded navigation from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="max-h-screen leading-normal lg:flex lg:items-center lg:pr-8">
-    <img
-      alt="Vue logo"
-      class="mx-auto mb-8 block lg:mb-0 lg:mr-8"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="flex flex-wrap items-start">
-      <nav class="mt-8 w-full text-center text-xs lg:mt-4 lg:ml-[-1rem] lg:text-left lg:text-base">
-        <RouterLink
-          to="/"
-          class="inline-block border-l border-border px-4 py-0 first:border-0 lg:py-4"
-        >
-          Home
-        </RouterLink>
-        <RouterLink
-          to="/about"
-          class="inline-block border-l border-border px-4 py-0 first:border-0 lg:py-4"
-        >
-          About
-        </RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
