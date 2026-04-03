a894d086b2303175d42e22678f67227fd6cc763e
AhogeK <ahogek@gmail.com>
2026-04-04 03:44:45 +0800

    feat(ui): add shadcn-vue sidebar, sheet, and skeleton components

---
+++ src/components/ui/sidebar/SidebarMenuSubItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <div
+    data-slot="sidebar-menu-sub-item"
+    data-sidebar="menu-sub-item"
+    :class="cn('group/menu-sub-item relative', props.class)"
+  >
+    <slot />
+  </div>
+</template>
