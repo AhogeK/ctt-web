7d87aa9c84aab694b23446a62d566f11cb5d3d86
AhogeK <ahogek@gmail.com>
2026-04-09 09:18:15 +0800

    chore: add Conventional Commits template and commitlint enforcement
    - .gitmessage: Conventional Commits template for git commit
    - commitlint.config.js: lint rules for commit message validation
    - commit-msg hook via simple-git-hooks (rejected husky, already using simple-git-hooks)
    - @commitlint/cli + @commitlint/config-conventional devDependencies

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert'],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-max-length': [2, 'always', 72],
+    'scope-case': [2, 'always', 'lower-case'],
+  },
+}
